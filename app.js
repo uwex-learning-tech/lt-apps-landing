4ac72eef7bfe7b20b1143efa6c721743805cfb67
Ethan Lin <ethan.lin@uwex.edu>
2020-11-06 12:57:10 -0600

    inital esspress app with routes
    and skeleton views

---
+++ app.js
@@ -0,0 +1,30 @@
+// IMPORTS
+const express = require( 'express' );
+const compression = require( 'compression' );
+const helmet = require( 'helmet' );
+
+// EXPRESS APP
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen( port );
+
+// use a middleware to compress response bodies for all requests
+app.use( compression() );
+
+// use a middleware to secure and set HTTP headers
+app.use( helmet() );
+
+// use the "public" directory on the file system to serve static files
+// including "apps"
+app.use( express.static( 'public' ) );
+
+// ROUTE: ROOT
+app.get( '/', ( req, res ) => {
+    res.render( 'index.ejs' );
+});
+
+// 404 - NO MORE CODE AFTER THIS STATEMENT
+app.use( ( req, res ) => {
+    res.status( 404 ).render( '404.ejs' );
+} );
